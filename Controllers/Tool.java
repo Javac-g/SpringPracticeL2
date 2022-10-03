49e2179a4a586b6e7f5c615c3bb001a76eb792a0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-03 17:15:36 +0300

    first

---
+++ Controllers/Tool.java
@@ -0,0 +1,22 @@
+package com.company.Controllers;
+
+public class Tool {
+
+    private String first, second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+}
