e0ca6a240f721388b1c06c2019db5942febc00f7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-03 17:27:38 +0300

    Third

--- Controllers/RequestDTO.java
+++ Controllers/RequestDTO.java
@@ -1,4 +1,43 @@
 package com.company.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RequestDTO {
+    private String name;
+    private Integer id;
+    private Tool tool;
+    private List<Car> carlist ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    public List<Car> getCarlist() {
+        return carlist;
+    }
+
+    public void setCarlist(List<Car> carlist) {
+        this.carlist = carlist;
+    }
 }
---
+++ Services/CarEnum.java
@@ -0,0 +1,7 @@
+package com.company.Services;
+
+public enum CarEnum {
+    AUDI,
+    FORD,
+    NONE
+}
