b12f617a185463b90d2358dcb5f8b32894b8ff5e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-10-03 18:42:37 +0300

    Config

---
+++ Configurations/SpringConfigurationFactory.java
@@ -0,0 +1,22 @@
+package com.company.Configurations;
+
+import com.company.Controllers.Controller;
+import com.company.Services.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+@Configuration
+public class SpringConfigurationFactory {
+
+    @Bean(name = "Controller")
+    @Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Controller controllerBean(){
+        return new Controller();
+    }
+    @Bean("Services")
+    public Services servicesBean(){
+        return new Services();
+    }
+}
